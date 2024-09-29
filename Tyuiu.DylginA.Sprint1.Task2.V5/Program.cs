a3905a882aff544b33451458649487eb8e8a273e
Anton Dylgin <Tyuiu.DylginA@outlook.com>
2024-09-29 17:58:57 +0500

    Task2

---
+++ Tyuiu.DylginA.Sprint1.Task2.V5/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.DylginA.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.DylginA.Sprint1.Task2.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1");
+            Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях");
+            Console.WriteLine("* Задание #2");
+            Console.WriteLine("* Вариант #5");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:");
+            Console.WriteLine("* Известна длина стороны куба. Вычислить площадь боковой поверхности куба.");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите длину стороны куба:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Площадь боковой поверхности равна:" + ds.CalculateSideSquare(x));
+        }
+    }
+}
